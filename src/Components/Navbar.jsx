e629230c570d54b0a2d167dfc33d405294f7164b
madankumarvmc <111742656+madankumarvmc@users.noreply.github.com>
2023-05-12 01:54:10 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-center">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">E-Store</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="d-flex justify-content-center align-items-center w-100">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
